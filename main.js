36ff4d87c20d39a60d533e94c3f18eaa8862671c
simonestrinati <simone.strinati@outlook.it>
2025-11-22 09:59:59 +0100

    creazione array

---
+++ main.js
@@ -0,0 +1,14 @@
+const automobili = [
+  { marca: "Fiat", modello: "Panda", alimentazione: "Benzina" },
+  { marca: "Volkswagen", modello: "Golf", alimentazione: "Diesel" },
+  { marca: "Tesla", modello: "Model 3", alimentazione: "Elettrica" },
+  { marca: "Toyota", modello: "Yaris", alimentazione: "Ibrida" },
+  { marca: "Renault", modello: "Clio", alimentazione: "GPL" },
+  { marca: "Ford", modello: "Focus", alimentazione: "Benzina" },
+  { marca: "BMW", modello: "Serie 1", alimentazione: "Diesel" },
+  { marca: "Hyundai", modello: "Kona", alimentazione: "Elettrica" },
+  { marca: "Audi", modello: "A3", alimentazione: "Ibrida" },
+  { marca: "Opel", modello: "Corsa", alimentazione: "Metano" }
+];
+
+
