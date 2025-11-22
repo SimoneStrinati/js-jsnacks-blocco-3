1b88f8f531253ac3b51e8d70c551a9852de40652
simonestrinati <simone.strinati@outlook.it>
2025-11-22 10:17:06 +0100

    snack 2 - lettere minuscole

--- main.js
+++ main.js
@@ -1,32 +1,45 @@
-const automobili = [
-  { marca: "Fiat", modello: "Panda", alimentazione: "Benzina" },
-  { marca: "Volkswagen", modello: "Golf", alimentazione: "Diesel" },
-  { marca: "Tesla", modello: "Model 3", alimentazione: "Elettrica" },
-  { marca: "Toyota", modello: "Yaris", alimentazione: "Ibrida" },
-  { marca: "Renault", modello: "Clio", alimentazione: "GPL" },
-  { marca: "Ford", modello: "Focus", alimentazione: "Benzina" },
-  { marca: "BMW", modello: "Serie 1", alimentazione: "Diesel" },
-  { marca: "Hyundai", modello: "Kona", alimentazione: "Elettrica" },
-  { marca: "Audi", modello: "A3", alimentazione: "Ibrida" },
-  { marca: "Opel", modello: "Corsa", alimentazione: "Metano" }
-];
+// const automobili = [
+//   { marca: "Fiat", modello: "Panda", alimentazione: "Benzina" },
+//   { marca: "Volkswagen", modello: "Golf", alimentazione: "Diesel" },
+//   { marca: "Tesla", modello: "Model 3", alimentazione: "Elettrica" },
+//   { marca: "Toyota", modello: "Yaris", alimentazione: "Ibrida" },
+//   { marca: "Renault", modello: "Clio", alimentazione: "GPL" },
+//   { marca: "Ford", modello: "Focus", alimentazione: "Benzina" },
+//   { marca: "BMW", modello: "Serie 1", alimentazione: "Diesel" },
+//   { marca: "Hyundai", modello: "Kona", alimentazione: "Elettrica" },
+//   { marca: "Audi", modello: "A3", alimentazione: "Ibrida" },
+//   { marca: "Opel", modello: "Corsa", alimentazione: "Metano" }
+// ];
 
-const benzina = automobili.filter(auto => {
+// const benzina = automobili.filter(auto => {
     
-    return (auto.alimentazione === "Benzina")
+//     return (auto.alimentazione === "Benzina")
     
-})
+// })
 
-const diesel = automobili.filter(auto => {
+// const diesel = automobili.filter(auto => {
     
-    return (auto.alimentazione === "Diesel")
-})
+//     return (auto.alimentazione === "Diesel")
+// })
 
-const altreAlimentazioni = automobili.filter(auto => {
+// const altreAlimentazioni = automobili.filter(auto => {
       
-    return (auto.alimentazione != "Benzina" && auto.alimentazione != "Diesel")
+//     return (auto.alimentazione != "Benzina" && auto.alimentazione != "Diesel")
+// })
+
+// console.log(benzina)
+// console.log(diesel)
+// console.log(altreAlimentazioni)
+
+
+/*******************Snack 2************************/
+
+const nomi = ["pippo", "PLUTO", "Paperino"];
+
+console.log(nomi)
+
+const nomiCorretti = nomi.map(nome => {
+    return lettereMinuscole = nome.toLowerCase();
 })
 
-console.log(benzina)
-console.log(diesel)
-console.log(altreAlimentazioni)
+console.log(nomiCorretti);
